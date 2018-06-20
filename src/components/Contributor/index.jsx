b7ae75a2bb90ecbea68a9d6d97dc0c345816c789
Noelle Daley <noelle@newrelic.com>
2018-06-20 09:09:47 -0700

    get number of additions and deletions

--- src/components/Contributor/index.jsx
+++ src/components/Contributor/index.jsx
@@ -3,16 +3,16 @@ import './styles.css'
 
 
 const Contributor = (contributorData) => {
-  const { avatar_url, html_url, username, commits } = contributorData
+  const { avatar_url, html_url, username, commits, additions, deletions } = contributorData
 
   return (
     <div className="Contributor">
       <img className="Contributor-avatar" src={avatar_url} alt="github avatar"/>
       <div>
         <a href={html_url} target="_blank">{username}</a>
-        <p>Commits: {commits}</p>
-        <p>Additions: {commits}</p>
-        <p>Deletions: {commits}</p>
+        <p>Commits: { commits }</p>
+        <p>Additions: { additions }</p>
+        <p>Deletions: { deletions }</p>
       </div>
     </div>
   )
--- src/components/ContributorList/index.jsx
+++ src/components/ContributorList/index.jsx
@@ -7,6 +7,8 @@ import './styles.css'
 // Add loading state
 // Decide if fetch should happen in service instead of component
 
+const GITHUB_CONTRIBUTORS_API_URL = 'https://api.github.com/repos/facebook/create-react-app/stats/contributors'
+
 class ContributorList extends Component {
   constructor(props) {
     super(props)
@@ -17,7 +19,7 @@ class ContributorList extends Component {
   }
 
   componentDidMount() {
-     fetch('https://api.github.com/repos/facebook/create-react-app/stats/contributors')
+     fetch(GITHUB_CONTRIBUTORS_API_URL)
         .then(response =>  response.json())
         .then(contributors => this.formatContributors(contributors))
         .then(formattedContributors => this.setState({contributors: formattedContributors}))
@@ -28,13 +30,23 @@ class ContributorList extends Component {
 
   formatContributors = (contributors) => {
     return contributors.map(c => {
+      let additions = 0
+      let deletions = 0
+
+      c.weeks.forEach(week => {
+        additions += week.a
+        deletions += week.d
+      })
+
       return (
         {
           id: c.author.id,
           avatar_url: c.author.avatar_url,
           html_url: c.author.html_url,
           username: c.author.login,
-          commits: c.total
+          commits: c.total,
+          additions,
+          deletions
         }
       )
     })
